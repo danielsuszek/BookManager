80d2d900ddf5f635d73df9f0c4bc32fd9cc588a8
Daniel Suszek <danielsuszek1@gmail.com>
2015-12-16 11:38:13 +0100

    Collecitons work

---
+++ smarty/templates_c/b722409a55ee776ac7375479d3ec1d0e42666243_6.file.config.conf.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version 3.1.28, created on 2015-12-14 15:34:47
+         compiled from "/home/daniel/Projects/BookManager/smarty/configs/config.conf" */ ?>
+<?php
+/* Smarty version 3.1.28, created on 2015-12-14 15:34:47
+  from "/home/daniel/Projects/BookManager/smarty/configs/config.conf" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.28',
+  'unifunc' => 'content_566ed387646954_67952137',
+  'file_dependency' => 
+  array (
+    'b722409a55ee776ac7375479d3ec1d0e42666243' => 
+    array (
+      0 => '/home/daniel/Projects/BookManager/smarty/configs/config.conf',
+      1 => 1450103626,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_566ed387646954_67952137 ($_smarty_tpl) {
+$_smarty_tpl->smarty->ext->configLoad->_loadConfigVars($_smarty_tpl, array (
+  'sections' => 
+  array (
+  ),
+  'vars' => 
+  array (
+    'APPLICATION_PATH' => '/Projects/BookManager/',
+  ),
+));
+}
+}
